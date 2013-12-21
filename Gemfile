885f01e7420fca6ab4db1c15cb84b5e1d048bbf6
Brian McAllister <brian@brianmcallister.com>
2013-12-21 18:11:36 -0500

    Set up user auth.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'sinatra', '~> 1.4.4'
 gem 'rerun', '~> 0.8.2'
+gem 'json_pure', '~> 1.8.1'
+gem 'patron', '~> 0.4.18'
--- server.rb
+++ server.rb
@@ -1,12 +1,40 @@
 require 'sinatra'
+require 'json'
+require 'patron'
 
-id = 'RNUOV5S2LW13D22K4WBXXVNXDF3TQ4NQZA5IOODCFI4XWYYZ'
-secret = 'SJTC0H15ZYCSDZU2HSWB1F3PBTEGC0YTGSK5ABBKITYXRY1B'
+configure do
+  set :port, '3000'
+  set :domain, "http://localhost:#{settings.port}"
+  
+  set :redirect, settings.domain
+  set :id, 'RNUOV5S2LW13D22K4WBXXVNXDF3TQ4NQZA5IOODCFI4XWYYZ'
+  set :secret, 'SJTC0H15ZYCSDZU2HSWB1F3PBTEGC0YTGSK5ABBKITYXRY1B'
+  
+  set :fsq_base_url, 'https://foursquare.com'
+  set :fsq_auth_url, 'oauth2/authenticate'
+  set :fsq_auth_params, "client_id=#{settings.id}" +
+    "&response_type=code&redirect_uri=#{settings.redirect}"
+end
+
+# http = Patron::Session.new
+# http.base_url = 
+
+code = ''
 
 get '/' do
-  'hellloo'
+  if params.has_key? 'code'
+    code = params['code']
+  end
+  
+  erb :index
+end
+
+get '/user' do
+  content_type :json
+  {'test' => 'hats', 'code' => code}.to_json
 end
 
 get '/auth' do
-  'auth'
+  redirect to "#{settings.fsq_base_url}/#{settings.fsq_auth_url}" +
+    "?#{settings.fsq_auth_params}"
 end
